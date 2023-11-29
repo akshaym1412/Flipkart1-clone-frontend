2068102d396f536fa66796f339df30b07231cead
Akshay_M <105048001+akshaym1412@users.noreply.github.com>
2023-11-29 23:25:32 +0530

    Update api.js

--- src/components/service/api.js
+++ src/components/service/api.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export const authenticateSignup=async(details)=>{
  try{
-    return await Axios.post(" https://flipkart-li8x.onrender.com/signup",details)
+    return await Axios.post("https://flipkart1-clone-backend1.vercel.app/api/signup",details)
 }
 catch(err){
   console.log("Error while calling signup api",err)
@@ -10,17 +10,17 @@ catch(err){
 
 export const authenticateLogin=async(details)=>{
     try{
-       return await Axios.post(" https://flipkart-li8x.onrender.com/login",details)
+       return await Axios.post("https://flipkart1-clone-backend1.vercel.app/api/login",details)
    }
    catch(err){
      console.log("Error while calling signup api",err)
    }}
 
 export const products=async()=>{
- const response = await Axios.get(" https://flipkart-li8x.onrender.com/products")
+ const response = await Axios.get("https://flipkart1-clone-backend1.vercel.app/api/products")
  return response.data;
 }
 
 export const addcart=async(data)=>{
-  return await Axios.post(" https://flipkart-li8x.onrender.com/cart",data);
+  return await Axios.post("https://flipkart1-clone-backend1.vercel.app/api/cart",data);
 }
